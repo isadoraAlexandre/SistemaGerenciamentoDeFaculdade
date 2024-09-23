23b9b00e2d3b9c0f63b955e0e57c77cc1ef40421
isadoraAlexandre <isadoraalexandre489@gmail.com>
2024-09-22 23:50:55 -0300

    Merge branch 'main' into calculo-ira

---
+++ src/main/java/com/mycompany/sistemagerenciamentodefaculdade/SistemaGerenciamentoDeFaculdade.java
@@ -0,0 +1,9 @@
+package com.mycompany.sistemagerenciamentodefaculdade;
+
+public class SistemaGerenciamentoDeFaculdade {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
