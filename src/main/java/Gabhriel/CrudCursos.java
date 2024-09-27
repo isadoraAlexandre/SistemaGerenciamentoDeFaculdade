8bf1527902473b4f0cf533aa2251c8b59bd41fb6
Gabhriel <gabhriel.velasco@estudante.ufjf.br>
2024-09-27 11:26:20 -0300

    add pasta gabhriel e coloquei todos os arquivos q criei e estavam sem pasta

--- src/main/java/Gabhriel/CrudCursos.java
+++ src/main/java/Gabhriel/CrudCursos.java
@@ -1,3 +1,4 @@
+package gabhriel;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
--- src/main/java/Gabhriel/VerHorario.java
+++ src/main/java/Gabhriel/VerHorario.java
@@ -1,3 +1,5 @@
+package gabhriel;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
